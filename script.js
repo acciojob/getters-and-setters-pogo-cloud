95a4aee3f64760f5cd95a3f451a3474d102f5265
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-10-18 12:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(this._name,this._age){
+		this._name=name;
+		this._age=age;
+	}
+	getName(){
+		return this._name;
+	}
+	setAge(value){
+		this._age=value;
+	}
 
-class Student extends Person {}
+    getAge(){
+		return this._age;
+	}
+	// study(){
+	// 	return `${this._name} is studying`;
+	// }
+	
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study(){
+		return `${this._name} is studying`;
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		return `${this._name} is teaching`;
+	}
+}
 
 // Do not change the code below this line
+const person = new Person("John", 25);
+console.log(person.name); // Output: John
+
+person.age = 30;
+console.log(person.age); // Output: 30
+
+const student = new Student("Alice", 22);
+student.study(); // Output: Alice is studying
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); // Output: Bob is teaching
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
